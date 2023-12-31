e72e6f9da6cbb56de57bed69ba5e7bd02bb0cdb2
devruhulamin <pythonruhul@gmail.com>
2023-12-31 18:30:25 +0600

    file thumbnail view

---
+++ lib/views/components/file_thumbnail_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/image_upload/models/thumbnail_request.dart';
+import 'package:instagram_clone/state/image_upload/providers/thumbnail_provider.dart';
+import 'package:instagram_clone/views/components/animations/loading_animation_view.dart';
+import 'package:instagram_clone/views/components/animations/small_error_animaion_view.dart';
+
+class FileThumbnailView extends ConsumerWidget {
+  final ThumbnailRequest thumbnailRequest;
+  const FileThumbnailView({super.key, required this.thumbnailRequest});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final thumbnail = ref.watch(thumbnailProvider(thumbnailRequest));
+    return thumbnail.when(
+      data: (imageWithAspectRation) {
+        return AspectRatio(
+          aspectRatio: imageWithAspectRation.aspectRatio,
+          child: imageWithAspectRation.image,
+        );
+      },
+      loading: () => const LoadingAnimationView(),
+      error: (error, stackTrace) => const SmallErrorAnimationView(),
+    );
+  }
+}

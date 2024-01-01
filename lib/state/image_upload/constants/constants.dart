92a99490aa10f66a82c243d26430ef6cc602b7b5
devruhulamin <pythonruhul@gmail.com>
2024-01-01 21:41:01 +0600

    add image upload constant

---
+++ lib/state/image_upload/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class ImageUploadConstant {
+  static const imageThumbnailWidth = 150;
+  static const videoThumbnailMaxHeight = 400;
+  static const videoThumbnailQuality = 70;
+  const ImageUploadConstant._();
+}

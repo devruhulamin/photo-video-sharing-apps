fdc34494c665532200c2a2937f6405f0884cedf0
devruhulamin <pythonruhul@gmail.com>
2024-01-04 09:11:49 +0600

    implement comment model

---
+++ lib/state/comment/models/comment_mode.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:instagram_clone/state/comment/type_defs/comment_type.dart';
+import 'package:instagram_clone/state/constant/firebase_field_name.dart';
+import 'package:instagram_clone/state/post/typedefs/post_id.dart';
+import 'package:instagram_clone/state/post/typedefs/user_id.dart';
+
+@immutable
+class CommentModel {
+  final CommentId commentId;
+  final UserId userId;
+  final PostId postId;
+  final DateTime createdAt;
+  final String comment;
+  CommentModel({required Map<String, dynamic> json, required String commentid})
+      : commentId = commentid,
+        userId = json[FirebaseFieldName.postId],
+        postId = json[FirebaseFieldName.postId],
+        createdAt = (json[FirebaseFieldName.createdAt] as Timestamp).toDate(),
+        comment = json[FirebaseFieldName.comment];
+  @override
+  operator ==(covariant CommentModel other) =>
+      identical(this, other) ||
+      (runtimeType == other.runtimeType &&
+          commentId == other.commentId &&
+          postId == other.postId &&
+          createdAt == other.createdAt);
+
+  @override
+  int get hashCode =>
+      Object.hashAll([runtimeType, commentId, postId, createdAt, userId]);
+}

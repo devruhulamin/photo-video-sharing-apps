76ec41513854a1787800e9cf037f5e60fffae49c
devruhulamin <pythonruhul@gmail.com>
2024-01-06 12:39:56 +0600

    add post or video view file

---
+++ lib/views/components/post/post_image_or_video_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/state/image_upload/models/file_type.dart';
+import 'package:instagram_clone/state/post/models/post.dart';
+import 'package:instagram_clone/views/components/post/post_image_view.dart';
+import 'package:instagram_clone/views/components/post/post_video_view.dart';
+
+class PostImageOrVideoView extends StatelessWidget {
+  final Post post;
+  const PostImageOrVideoView({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return () {
+      switch (post.fileType) {
+        case FileType.image:
+          return PostImageView(post: post);
+        case FileType.video:
+          return PostVideoView(post: post);
+        default:
+          return const SizedBox();
+      }
+    }();
+  }
+}

a0ef16c09963e594ddbaa6baba24c157d97fca5a
devruhulamin <pythonruhul@gmail.com>
2024-01-17 21:33:06 +0600

    added reels payload

---
+++ lib/state/reels_upload/model/reels_payload.dart
@@ -0,0 +1,31 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:instagram_clone/state/post/models/post_key.dart';
+
+import 'package:instagram_clone/state/post/typedefs/user_id.dart';
+import 'package:instagram_clone/state/post_settings/models/post_setting.dart';
+
+@immutable
+class ReelsPayload extends MapView<String, dynamic> {
+  ReelsPayload({
+    required String fileUrl,
+    required UserId userId,
+    required String message,
+    required String fileName,
+    required String originalFileStorageId,
+    required double aspectRatio,
+    required Map<PostSetting, bool> postSettings,
+  }) : super({
+          PostKey.userId: userId,
+          PostKey.fileName: fileName,
+          PostKey.message: message,
+          PostKey.originalFileStorageId: originalFileStorageId,
+          PostKey.aspectRatio: aspectRatio,
+          PostKey.fileUrl: fileUrl,
+          PostKey.postSettings: {
+            for (final postSetting in postSettings.entries)
+              postSetting.key.storageKey: postSetting.value,
+          }
+        });
+}
